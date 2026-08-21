0e672c7ffd02db7da7b13c1defe623eeec9ecc61
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-08-21 05:24:47 -0400

    docs: add strict Symbolics API QA
    Add a SciMLTesting 2.4 QA entry point and render the public API names that the strict documentation check finds. Correct stale import owners and update the PreallocationTools extension for current cache storage.
    
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- ext/SymbolicsPreallocationToolsExt.jl
+++ ext/SymbolicsPreallocationToolsExt.jl
@@ -4,46 +4,47 @@ using PreallocationTools
 import PreallocationTools: _restructure, get_tmp
 using Symbolics, ForwardDiff
 
-function get_tmp(dc::DiffCache, u::Type{X}) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
-    if length(dc.du) > length(dc.any_du)
-        resize!(dc.any_du, length(dc.du))
+function _symbolics_get_tmp(dc, ::Type{X}) where {X}
+    if hasfield(typeof(dc), :any_du)
+        any_du = getfield(dc, :any_du)
+        if length(dc.du) > length(any_du)
+            resize!(any_du, length(dc.du))
+        end
+        return _restructure(dc.du, any_du)
     end
-    _restructure(dc.du, dc.any_du)
+
+    typed_du = getfield(dc, :typed_du)
+    buf = get!(typed_du, X) do
+        similar(dc.du, X, length(dc.du))
+    end
+    if length(buf) != length(dc.du)
+        buf = typed_du[X] = similar(dc.du, X, length(dc.du))
+    end
+    return _restructure(dc.du, buf)
 end
 
-function get_tmp(dc::DiffCache, u::X) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
-    if length(dc.du) > length(dc.any_du)
-        resize!(dc.any_du, length(dc.du))
-    end
-    _restructure(dc.du, dc.any_du)
+function get_tmp(dc::DiffCache, ::Type{X}) where {T, N, X <: ForwardDiff.Dual{T, Num, N}}
+    return _symbolics_get_tmp(dc, X)
 end
 
-function get_tmp(dc::DiffCache, u::AbstractArray{X}) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
-    if length(dc.du) > length(dc.any_du)
-        resize!(dc.any_du, length(dc.du))
-    end
-    _restructure(dc.du, dc.any_du)
+function get_tmp(dc::DiffCache, ::X) where {T, N, X <: ForwardDiff.Dual{T, Num, N}}
+    return _symbolics_get_tmp(dc, X)
 end
 
-function get_tmp(dc::FixedSizeDiffCache, u::Type{X}) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
-    if length(dc.du) > length(dc.any_du)
-        resize!(dc.any_du, length(dc.du))
-    end
-    _restructure(dc.du, dc.any_du)
+function get_tmp(dc::DiffCache, ::AbstractArray{X}) where {T, N, X <: ForwardDiff.Dual{T, Num, N}}
+    return _symbolics_get_tmp(dc, X)
 end
 
-function get_tmp(dc::FixedSizeDiffCache, u::X) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
-    if length(dc.du) > length(dc.any_du)
-        resize!(dc.any_du, length(dc.du))
-    end
-    _restructure(dc.du, dc.any_du)
+function get_tmp(dc::FixedSizeDiffCache, ::Type{X}) where {T, N, X <: ForwardDiff.Dual{T, Num, N}}
+    return _symbolics_get_tmp(dc, X)
 end
 
-function get_tmp(dc::FixedSizeDiffCache, u::AbstractArray{X}) where {T,N, X<: ForwardDiff.Dual{T, Num, N}}
-    if length(dc.du) > length(dc.any_du)
-        resize!(dc.any_du, length(dc.du))
-    end
-    _restructure(dc.du, dc.any_du)
+function get_tmp(dc::FixedSizeDiffCache, ::X) where {T, N, X <: ForwardDiff.Dual{T, Num, N}}
+    return _symbolics_get_tmp(dc, X)
+end
+
+function get_tmp(dc::FixedSizeDiffCache, ::AbstractArray{X}) where {T, N, X <: ForwardDiff.Dual{T, Num, N}}
+    return _symbolics_get_tmp(dc, X)
 end
 
 end
